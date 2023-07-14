c7aa0c87c73dcd5648bacf2ea5a338a22d017271
MsEmily1020 <peace10200khs@gmail.com>
2023-07-14 21:44:11 +0900

    [Docs] Tool Tip 주석 추가

--- src/tooltiptext/CountryButtonToolTipText.java
+++ src/tooltiptext/CountryButtonToolTipText.java
@@ -6,13 +6,20 @@ import java.io.FileReader;
 
 import list.ButtonList;
 
+/**
+ * 각 보드 판의 설명 tool tip을 보여주는 클래스입니다.
+ * 
+ * @author MsEmily1020
+ * @version 1.0
+ *
+ */
 public class CountryButtonToolTipText {
 	public CountryButtonToolTipText() {
-		var buttonList = ButtonList.getArr();
+		var buttonList = ButtonList.getArr();	// 버튼의 리스트
 		int i = 0;
 		try {
 			BufferedReader bufReader = new BufferedReader(
-					new FileReader(new File("./datafiles/CountryButtonToolTipList.txt")));
+					new FileReader(new File("./datafiles/CountryButtonToolTipList.txt")));	// tool tip에 넣을 txt 내용 가져옴
 			String line = "";
 			while ((line = bufReader.readLine()) != null) {
 				buttonList.get(i++).setToolTipText(line);
