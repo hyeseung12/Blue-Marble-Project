5cfcd07a65147acd3955b404a181244dea1c5d64
MsEmily1020 <peace10200khs@gmail.com>
2023-07-17 12:05:33 +0900

    [Refactor] DicePanel method 구조화

--- src/dice/DicePanel.java
+++ src/dice/DicePanel.java
@@ -21,9 +21,20 @@ import list.DiceNumberList;
  */
 public class DicePanel extends JPanel {
 	private static JButton[] diceBtn = new JButton[2];
+	private final static int IMAGESIZE = 190;
 
 	public DicePanel() {
 		setLayout(new FlowLayout(FlowLayout.CENTER, 48, 30));
+		
+		initDiceButton();		// 주사위 생성
+		randomDiceNumber(); 	// 랜덤으로 숫자 돌리기
+		randomThreadDice();		// 랜덤으로 주사위 돌리기
+	}
+
+	/**
+	 * 주사위를 생성 및 초기화하는 메소드입니다.
+	 */
+	private void initDiceButton() {
 		for (int i = 0; i < 2; i++) {
 			diceBtn[i] = new JButton();
 			diceBtn[i].setOpaque(true);
@@ -33,60 +44,69 @@ public class DicePanel extends JPanel {
 			diceBtn[i].setPreferredSize(new Dimension(100, 100));
 			add(diceBtn[i]);
 		}
+	}
+	
+	/**
+	 * 랜덤으로 주사위 숫자를 정하는 메소드입니다.
+	 */
+	private void randomDiceNumber() {
+		int diceNum1 = (int) (Math.random() * 6 + 1);
+		int diceNum2 = (int) (Math.random() * 6 + 1);
 
-		randomThreadDice();
+		DiceNumberList.setDiceNum1(diceNum1);
+		DiceNumberList.setDiceNum2(diceNum2);
 	}
 
 	/**
-	 * 주사위를 랜덤으로 돌리는 메소드입니다.
+	 * 주사위를 랜덤으로 돌리는 애니메이션 메소드입니다.
 	 */
 	public static void randomThreadDice() {
-		int imgSize = 190;
+	    Timer timer = new Timer();
+	    int delay = 100; // 이미지 변경 간격 (0.1초)
+	    int duration = 1100; // 전체 동작 시간 (1.1초)
+	    int steps = duration / delay; // 이미지 변경 횟수
 
-		int diceNum1 = (int)(Math.random() * 6 + 1);
-		int diceNum2 = (int)(Math.random() * 6 + 1);
+	    TimerTask task = new TimerTask() {
+	        int stepCount = 0;
+
+	        @Override
+	        public void run() {
+	            if (stepCount < steps) {
+	                // 첫 번째 주사위 이미지 무작위로 선택 후 버튼에 적용
+	                int randomImageIndex1 = (int) (Math.random() * 6 + 1);
+	                int randomImageIndex2 = (int) (Math.random() * 6 + 1);
+	                Image img1 = new ImageIcon("./images/dice" + randomImageIndex1 + ".png").getImage()
+	                        .getScaledInstance(IMAGESIZE, IMAGESIZE, java.awt.Image.SCALE_SMOOTH);
+	                Image img2 = new ImageIcon("./images/dice" + randomImageIndex2 + ".png").getImage()
+	                        .getScaledInstance(IMAGESIZE, IMAGESIZE, java.awt.Image.SCALE_SMOOTH);
+	                diceBtn[0].setIcon(new ImageIcon(img1));
+	                diceBtn[1].setIcon(new ImageIcon(img2));
+	                stepCount++;
+	            } else {
+	            	setFinalButtonImage();	// 최종 숫자의 주사위 보여주기
+	                cancel();
+	                CountryDetailPanel.diceClickBtn.setVisible(true);
+	            }
+	        }
+	    };
+
+	    // 이미지 변경 간격(delay)마다 TimerTask 실행
+	    timer.schedule(task, delay, delay);
+	}
+
+	/**
+	 * 최종 숫자의 주사위를 보여주는 메소드입니다.
+	 */
+	private static void setFinalButtonImage() {
+		Image[] diceFinalImages = {
+				new ImageIcon("./images/dice" + DiceNumberList.getDiceNum1() + ".png").getImage()
+						.getScaledInstance(IMAGESIZE, IMAGESIZE, java.awt.Image.SCALE_SMOOTH),
+
+				new ImageIcon("./images/dice" + DiceNumberList.getDiceNum2() + ".png").getImage()
+						.getScaledInstance(IMAGESIZE, IMAGESIZE, java.awt.Image.SCALE_SMOOTH) };
 		
-		DiceNumberList.setDiceNum1(diceNum1);
-		DiceNumberList.setDiceNum2(diceNum2);
-		
-		Image[] img = {
-				new ImageIcon("./images/dice" + diceNum1 + ".png").getImage().getScaledInstance(imgSize, imgSize,
-						java.awt.Image.SCALE_SMOOTH),
-
-				new ImageIcon("./images/dice" + diceNum2 + ".png").getImage().getScaledInstance(imgSize, imgSize,
-						java.awt.Image.SCALE_SMOOTH) };
-
-		Timer timer = new Timer();
-		int delay = 100; // 이미지 변경 간격 (0.1초)
-		int duration = 1100; // 전체 동작 시간 (1.1초)
-		int steps = duration / delay; // 이미지 변경 횟수
-
-		TimerTask task = new TimerTask() {
-			int stepCount = 0;
-
-			@Override
-			public void run() {
-				if (stepCount < steps) {
-					// 이미지 무작위로 선택 후 버튼에 적용
-					int randomImageIndex = (int) (Math.random() * 6 + 1);
-					Image img = new ImageIcon("./images/dice" + randomImageIndex + ".png").getImage()
-							.getScaledInstance(imgSize, imgSize, java.awt.Image.SCALE_SMOOTH);
-					diceBtn[0].setIcon(new ImageIcon(img));
-					diceBtn[1].setIcon(new ImageIcon(img));
-					stepCount++;
-				} else {
-					// 최종 이미지 선택 후 버튼에 적용
-					diceBtn[0].setIcon(new ImageIcon(img[0]));
-					diceBtn[1].setIcon(new ImageIcon(img[1]));
-
-					// TimerTask 종료
-					cancel();
-					CountryDetailPanel.diceClickBtn.setVisible(true);
-				}
-			}
-		};
-
-		// 이미지 변경 간격(delay)마다 TimerTask 실행
-		timer.schedule(task, delay, delay);
+		// 최종 이미지 선택 후 버튼에 적용
+		diceBtn[0].setIcon(new ImageIcon(diceFinalImages[0]));
+		diceBtn[1].setIcon(new ImageIcon(diceFinalImages[1]));
 	}
 }
